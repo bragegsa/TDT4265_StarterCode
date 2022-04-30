4d0299e8b31536f64cf288b07539e51fdd629407
bragegsa <bragegsa@stud.ntnu.no>
2022-04-30 14:14:03 +0200

    removed maxpool

--- assignment4/SSD/ssd/modeling/backbones/retinanet_backbone.py
+++ assignment4/SSD/ssd/modeling/backbones/retinanet_backbone.py
@@ -85,7 +85,7 @@ class Resnet50WithFPN(torch.nn.Module):
         x = self.conv(x)
         x = self.bn1(x)
         x = self.relu(x)
-        x = self.maxpool(x)
+        # x = self.maxpool(x)
 
         x = self.conv1(x)
         out_features.append(x)
