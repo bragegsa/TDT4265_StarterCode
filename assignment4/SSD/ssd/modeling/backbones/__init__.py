236cef1f2423b7335d879734dd35fdd95cd77be8
bragegsa <bragegsa@stud.ntnu.no>
2022-04-30 12:43:49 +0200

    Changes to init

--- assignment4/SSD/ssd/modeling/backbones/__init__.py
+++ assignment4/SSD/ssd/modeling/backbones/__init__.py
@@ -1,4 +1,4 @@
 from .basic import BasicModel
 from .retinanet_backbone import Resnet50WithFPN
-from .resnet101_fpn import Resnet101WithFPN
+# from .resnet101_fpn import Resnet101WithFPN
 from .vgg import VGG
