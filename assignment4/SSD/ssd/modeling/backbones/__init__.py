e69c3046c8efd53df579061a7df4f2ab4ee8830c
bragegsa <bragegsa@stud.ntnu.no>
2022-05-02 21:53:13 +0200

    updated backbones init.py

--- assignment4/SSD/ssd/modeling/backbones/__init__.py
+++ assignment4/SSD/ssd/modeling/backbones/__init__.py
@@ -1,5 +1,3 @@
 from .basic import BasicModel
-from .retinanet_backbone import Resnet50WithFPN
-# from .resnet101_fpn import Resnet101WithFPN
-from .fpn_backbone_suggestion import FPN
+from .resnetFPN import resnetFPN
 from .vgg import VGG
