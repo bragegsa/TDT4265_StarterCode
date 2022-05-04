f65d1af9966909ee84e2f9f0a62829bfda1d80f1
bragegsa <bragegsa@stud.ntnu.no>
2022-05-04 17:21:02 +0200

    Added anchors

--- assignment4/SSD/configs/tdt4265_augmented_config.py
+++ assignment4/SSD/configs/tdt4265_augmented_config.py
@@ -7,7 +7,6 @@ from ssd.data.transforms import (
     ToTensor, Normalize, Resize,
     GroundTruthBoxesToAnchors, RandomHorizontalFlip, RandomSampleCrop, ColorJitter)
 from .ssd300 import train, optimizer, schedulers, backbone, model, data_train, data_val, loss_objective
-from .test_anchors.base import anchors 
 from .utils import get_dataset_dir
 
 # Keep the model, except change the backbone and number of classes
@@ -48,3 +47,19 @@ data_val.gpu_transform = gpu_transform
 data_train.gpu_transform = gpu_transform
 
 label_map = {idx: cls_name for idx, cls_name in enumerate(TDT4265Dataset.class_names)}
+
+anchors = L(AnchorBoxes)(
+    feature_sizes= [[32, 256], [16, 128], [8, 64], [4, 32], [2, 16], [1, 8]],
+    # Strides is the number of pixels (in image space) between each spatial position in the feature map
+    strides= [[4, 4], [8, 8], [16, 16], [32, 32], [64, 64], [128, 128]],
+    min_sizes= [[16, 16], [32, 32], [48, 48], [64, 64], [86, 86], [128, 128], [128, 400]],
+    # Strides is the number of pixels (in image space) between each spatial position in the feature map
+    # aspect ratio is defined per feature map (first index is largest feature map (38x38))
+    # aspect ratio is used to define two boxes per element in the list.
+    # if ratio=[2], boxes will be created with ratio 1:2 and 2:1
+    # Number of boxes per location is in total 2 + 2 per aspect ratio
+    aspect_ratios= [[2, 3], [2, 3], [2, 3], [2, 3], [2], [2]],
+    image_shape="${train.imshape}",
+    scale_center_variance=0.1,
+    scale_size_variance=0.2
+)
