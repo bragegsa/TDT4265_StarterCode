925074cf84487e9857033925245dc1bdd28e7065
bragegsa <bragegsa@stud.ntnu.no>
2022-05-04 17:28:41 +0200

    fixed anchors in tdt4265_augmented_2_config.py

--- assignment4/SSD/configs/tdt4265_augmented_2_config.py
+++ assignment4/SSD/configs/tdt4265_augmented_2_config.py
@@ -6,7 +6,7 @@ from ssd.data.transforms import (
     ToTensor, Normalize, Resize,
     GroundTruthBoxesToAnchors, RandomHorizontalFlip, RandomSampleCrop, ColorJitter)
 from .ssd300 import train, optimizer, schedulers, backbone, model, data_train, data_val, loss_objective
-from .test_anchors.base import anchors 
+from .tdt4265_augmented_config import anchors 
 from .utils import get_dataset_dir
 
 # Keep the model, except change the backbone and number of classes
