622f22f8d097a8955d12e03bd7b87290717d7304
Niklas Strømsnes <niklast@stud.ntnu.no>
2022-05-05 23:52:01 +0200

    Formatting anchro box parameters

--- assignment4/SSD/configs/exploration_knowledge_config.py
+++ assignment4/SSD/configs/exploration_knowledge_config.py
@@ -17,19 +17,19 @@ from ssd.modeling import AnchorBoxes
 
 anchors = L(AnchorBoxes)(
     # Parameters:
-    #     feature_sizes:  Dimensions of the anchor box placement grids.
-    #     strides:        The number of pixels between each spatial position in the feature map
-    #     min_sizes:      (?) Lower bound for anchor box sizes
+    #     feature_sizes:  Dimensions of the anchor box placement grids
     #     aspect_ratios:  Defines two boxes per element in the list, corresponding to maps in feature_maps
     #                     For example, if ratio=[2], boxes will be created with ratio 1:2 and 2:1
     #                     Then the number of boxes per location is in total 2 + 2 per aspect ratio
+    #     strides:        The number of pixels between each spatial position in the feature map
+    #     min_sizes:      (?) Lower bound for anchor box sizes
     #
     # [vertical, horizontal], counting from bottom up and left to right
 
-    feature_sizes = [[32, 256], [16, 128], [8, 64], [4, 32], [2, 16], [1, 8]],
-    strides = [[4, 3], [8, 5], [16, 11], [32, 21], [64, 43], [64, 128]],
-    min_sizes = [[10, 10], [15, 15], [25, 25], [40, 40], [60, 60], [90, 90], [128, 128]],
-    aspect_ratios = [[2, 3], [2, 3], [2, 3], [2, 3], [2, 3], [2, 3, 4]],
+    feature_sizes = [[32, 256], [16, 128],  [8, 64],    [4, 32],    [2, 16],    [1, 8]],
+    aspect_ratios = [[2, 3],    [2],        [2, 3],     [2, 3],     [2, 3],     [2, 3, 4]],
+    strides =       [[4, 3],    [8, 5],     [16, 11],   [32, 21],   [64, 43],   [64, 128]],
+    min_sizes =     [[50, 50],  [50, 50],   [50, 50],   [50, 50],   [50, 50],   [50, 50],   [50, 50]],
     image_shape = "${train.imshape}",
     scale_center_variance = 0.1,
     scale_size_variance = 0.2
