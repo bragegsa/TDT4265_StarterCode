7c05ec9b5ae89534142a0f19231ec611f77f58d2
Niklas Strømsnes <niklast@stud.ntnu.no>
2022-05-05 18:05:04 +0200

    Changed comment format because multiline comment no work

--- assignment4/SSD/configs/exploration_knowledge_config.py
+++ assignment4/SSD/configs/exploration_knowledge_config.py
@@ -16,21 +16,20 @@ from tops.config import LazyCall as L
 from ssd.modeling import AnchorBoxes
 
 anchors = L(AnchorBoxes)(
-    """Defines anchor boxes.
-        Parameters:
-            feature_sizes:  Dimensions of the anchor box placement grids.
-            strides:        The number of pixels between each spatial position in the feature map
-            min_sizes:      (?) Lower bound for anchor box sizes
-            aspect_ratios:  Defines two boxes per element in the list, corresponding to maps in feature_maps
-                            For example, if ratio=[2], boxes will be created with ratio 1:2 and 2:1
-                            Then the number of boxes per location is in total 2 + 2 per aspect ratio
-        
-    [vertical, horizontal], counting from bottom up and left to right
-    """
+    # Parameters:
+    #     feature_sizes:  Dimensions of the anchor box placement grids.
+    #     strides:        The number of pixels between each spatial position in the feature map
+    #     min_sizes:      (?) Lower bound for anchor box sizes
+    #     aspect_ratios:  Defines two boxes per element in the list, corresponding to maps in feature_maps
+    #                     For example, if ratio=[2], boxes will be created with ratio 1:2 and 2:1
+    #                     Then the number of boxes per location is in total 2 + 2 per aspect ratio
+    #
+    # [vertical, horizontal], counting from bottom up and left to right
+    
     feature_sizes=[[32, 256], [16, 128], [8, 64], [4, 32], [2, 16], [1, 8]],
     strides=[[4, 3], [8, 5], [16, 11], [32, 21], [64, 43], [64, 128]],
-    min_sizes=[[8, 8], [16, 8], [32, 32], [64, 64], [86, 86], [85, 85], [128, 256]],
-    aspect_ratios=[[2, 3], [2, 3], [2, 3], [2, 3], [2], [2, 3, 4]],
+    min_sizes=[[8, 8], [16, 16], [32, 32], [64, 64], [32, 64], [128, 128], [128, 256]],
+    aspect_ratios=[[2, 3], [2, 3], [2, 3], [2, 3], [2, 3], [2, 3, 4]],
     image_shape="${train.imshape}",
     scale_center_variance=0.1,
     scale_size_variance=0.2
