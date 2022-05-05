3259783073982e0c7857e61e1f43a3add83c1525
Niklas Strømsnes <niklast@stud.ntnu.no>
2022-05-05 18:06:22 +0200

    Readbility is nice

--- assignment4/SSD/configs/exploration_knowledge_config.py
+++ assignment4/SSD/configs/exploration_knowledge_config.py
@@ -26,11 +26,11 @@ anchors = L(AnchorBoxes)(
     #
     # [vertical, horizontal], counting from bottom up and left to right
     
-    feature_sizes=[[32, 256], [16, 128], [8, 64], [4, 32], [2, 16], [1, 8]],
-    strides=[[4, 3], [8, 5], [16, 11], [32, 21], [64, 43], [64, 128]],
-    min_sizes=[[8, 8], [16, 16], [32, 32], [64, 64], [32, 64], [128, 128], [128, 256]],
-    aspect_ratios=[[2, 3], [2, 3], [2, 3], [2, 3], [2, 3], [2, 3, 4]],
-    image_shape="${train.imshape}",
-    scale_center_variance=0.1,
-    scale_size_variance=0.2
+    feature_sizes = [[32, 256], [16, 128], [8, 64], [4, 32], [2, 16], [1, 8]],
+    strides = [[4, 3], [8, 5], [16, 11], [32, 21], [64, 43], [64, 128]],
+    min_sizes = [[8, 8], [16, 16], [32, 32], [64, 64], [32, 64], [128, 128], [128, 256]],
+    aspect_ratios = [[2, 3], [2, 3], [2, 3], [2, 3], [2, 3], [2, 3, 4]],
+    image_shape = "${train.imshape}",
+    scale_center_variance = 0.1,
+    scale_size_variance = 0.2
 )
